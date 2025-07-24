3b4897e9563a842680f751cfb3556fbec8c53f63
beetashoke <beetashoke.chakraborty@clapgrow.com>
2025-07-24 15:43:48 +0530

    amount fixed

--- shiw/shiw/doctype/heat/heat.js
+++ shiw/shiw/doctype/heat/heat.js
@@ -1,8 +1,8 @@
 // Copyright (c) 2025, beetashoke chakraborty and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on("Heat", {
-    refresh(frm) {
-        console.log("hiii")
-    },
-});
+// frappe.ui.form.on("Heat", {
+//     refresh(frm) {
+//         console.log("hiii")
+//     },
+// });
--- shiw/shiw/report/comprehensive_summary_report/comprehensive_summary_report.py
+++ shiw/shiw/report/comprehensive_summary_report/comprehensive_summary_report.py
@@ -59,104 +59,6 @@
 # 	return columns, data
 
 
-import frappe
-import calendar
-
-
-def execute(filters=None):
-	if not filters:
-		return [], []
-
-	month = filters.get("month")
-	year = int(filters.get("year"))
-	month_index = list(calendar.month_name).index(month)
-	days_in_month = calendar.monthrange(year, month_index)[1]
-
-	# Column headers
-	columns = ["Description"] + [f"{day}-{month[:3]}" for day in range(1, days_in_month + 1)]
-
-	def get_date(day):
-		return f"{year}-{str(month_index).zfill(2)}-{str(day).zfill(2)}"
-
-	data = []
-
-	# Row 1: Raw Materials Received (Accepted Quantity from Purchase Receipt Items)
-	row_raw_materials = ["Raw Materials Received"]
-	for day in range(1, days_in_month + 1):
-		date_str = get_date(day)
-
-		accepted_qty = (
-			frappe.db.sql(
-				"""
-				SELECT SUM(pri.qty)
-				FROM `tabPurchase Receipt` pr
-				JOIN `tabPurchase Receipt Item` pri ON pri.parent = pr.name
-				JOIN `tabItem` i ON i.name = pri.item_code
-				WHERE pr.posting_date = %s
-				  AND i.item_group = 'Raw Material'
-				""",
-				(date_str,),
-				as_list=True,
-			)[0][0]
-			or 0
-		)
-
-		row_raw_materials.append(accepted_qty)
-
-	data.append(row_raw_materials)
-
-	# Row 2: Raw Materials Melted (from Heat Doctype)
-	row_rm_melted = ["Raw Materials Melted"]
-	for day in range(1, days_in_month + 1):
-		date_str = get_date(day)
-
-		melted_qty = (
-			frappe.db.sql(
-				"""
-				SELECT SUM(total_charge_mix_in_kg)
-				FROM `tabHeat`
-				WHERE `date` = %s
-				""",
-				(date_str,),
-				as_list=True,
-			)[0][0]
-			or 0
-		)
-
-		row_rm_melted.append(melted_qty)
-
-	data.append(row_rm_melted)
-
-	# Row 3: Raw Materials Stock (as of each day, from Stock Ledger Entry)
-	row_rm_stock = ["Raw Materials Stock"]
-
-	for day in range(1, days_in_month + 1):
-		date_str = get_date(day)
-
-		stock_qty = (
-			frappe.db.sql(
-				"""
-				SELECT SUM(sle.actual_qty)
-				FROM `tabStock Ledger Entry` sle
-				JOIN `tabItem` i ON sle.item_code = i.name
-				WHERE sle.posting_date <= %s
-				AND sle.warehouse = 'Stores - SHIW'
-				AND i.item_group = 'Raw Material'
-				AND sle.is_cancelled = 0
-				""",
-				(date_str,),
-				as_list=True,
-			)[0][0]
-			or 0
-		)
-
-		row_rm_stock.append(stock_qty)
-
-	data.append(row_rm_stock)
-
-	return columns, data
-
-
 # import frappe
 # import calendar
 
@@ -178,10 +80,11 @@ def execute(filters=None):
 
 # 	data = []
 
-# 	# Row 1: Raw Materials Received
+# 	# Row 1: Raw Materials Received (Accepted Quantity from Purchase Receipt Items)
 # 	row_raw_materials = ["Raw Materials Received"]
 # 	for day in range(1, days_in_month + 1):
 # 		date_str = get_date(day)
+
 # 		accepted_qty = (
 # 			frappe.db.sql(
 # 				"""
@@ -197,33 +100,39 @@ def execute(filters=None):
 # 			)[0][0]
 # 			or 0
 # 		)
+
 # 		row_raw_materials.append(accepted_qty)
+
 # 	data.append(row_raw_materials)
 
-# 	# Row 2: Raw Materials Melted
+# 	# Row 2: Raw Materials Melted (from Heat Doctype)
 # 	row_rm_melted = ["Raw Materials Melted"]
 # 	for day in range(1, days_in_month + 1):
 # 		date_str = get_date(day)
+
 # 		melted_qty = (
 # 			frappe.db.sql(
 # 				"""
 # 				SELECT SUM(total_charge_mix_in_kg)
 # 				FROM `tabHeat`
 # 				WHERE `date` = %s
-# 				AND docstatus = 1
 # 				""",
 # 				(date_str,),
 # 				as_list=True,
 # 			)[0][0]
 # 			or 0
 # 		)
+
 # 		row_rm_melted.append(melted_qty)
+
 # 	data.append(row_rm_melted)
 
-# 	# Row 3: Raw Materials Stock
+# 	# Row 3: Raw Materials Stock (as of each day, from Stock Ledger Entry)
 # 	row_rm_stock = ["Raw Materials Stock"]
+
 # 	for day in range(1, days_in_month + 1):
 # 		date_str = get_date(day)
+
 # 		stock_qty = (
 # 			frappe.db.sql(
 # 				"""
@@ -240,30 +149,121 @@ def execute(filters=None):
 # 			)[0][0]
 # 			or 0
 # 		)
+
 # 		row_rm_stock.append(stock_qty)
+
 # 	data.append(row_rm_stock)
 
-# 	# Row 4: Liquid Metal Generated
-# 	row_liquid_metal = ["Liquid Metal Generated"]
-# 	for day in range(1, days_in_month + 1):
-# 		date_str = get_date(day)
-# 		liquid_qty = (
-# 			frappe.db.sql(
-# 				"""
-# 				SELECT SUM(liquid_balence)
-# 				FROM `tabHeat`
-# 				WHERE `date` = %s
-# 				AND docstatus = 1
-# 				""",
-# 				(date_str,),
-# 				as_list=True,
-# 			)[0][0]
-# 			or 0
-# 		)
-# 		row_liquid_metal.append(liquid_qty)
-# 	data.append(row_liquid_metal)
-
 # 	return columns, data
+
+
+import frappe
+import calendar
+
+
+def execute(filters=None):
+	if not filters:
+		return [], []
+
+	month = filters.get("month")
+	year = int(filters.get("year"))
+	month_index = list(calendar.month_name).index(month)
+	days_in_month = calendar.monthrange(year, month_index)[1]
+
+	# Column headers
+	columns = ["Description"] + [f"{day}-{month[:3]}" for day in range(1, days_in_month + 1)]
+
+	def get_date(day):
+		return f"{year}-{str(month_index).zfill(2)}-{str(day).zfill(2)}"
+
+	data = []
+
+	# Row 1: Raw Materials Received
+	row_raw_materials = ["Raw Materials Received"]
+	for day in range(1, days_in_month + 1):
+		date_str = get_date(day)
+		accepted_qty = (
+			frappe.db.sql(
+				"""
+				SELECT SUM(pri.qty)
+				FROM `tabPurchase Receipt` pr
+				JOIN `tabPurchase Receipt Item` pri ON pri.parent = pr.name
+				JOIN `tabItem` i ON i.name = pri.item_code
+				WHERE pr.posting_date = %s
+				  AND i.item_group = 'Raw Material'
+				""",
+				(date_str,),
+				as_list=True,
+			)[0][0]
+			or 0
+		)
+		row_raw_materials.append(accepted_qty)
+	data.append(row_raw_materials)
+
+	# Row 2: Raw Materials Melted
+	row_rm_melted = ["Raw Materials Melted"]
+	for day in range(1, days_in_month + 1):
+		date_str = get_date(day)
+		melted_qty = (
+			frappe.db.sql(
+				"""
+				SELECT SUM(total_charge_mix_in_kg)
+				FROM `tabHeat`
+				WHERE `date` = %s
+				AND docstatus = 1
+				""",
+				(date_str,),
+				as_list=True,
+			)[0][0]
+			or 0
+		)
+		row_rm_melted.append(melted_qty)
+	data.append(row_rm_melted)
+
+	# Row 3: Raw Materials Stock
+	row_rm_stock = ["Raw Materials Stock"]
+	for day in range(1, days_in_month + 1):
+		date_str = get_date(day)
+		stock_qty = (
+			frappe.db.sql(
+				"""
+				SELECT SUM(sle.actual_qty)
+				FROM `tabStock Ledger Entry` sle
+				JOIN `tabItem` i ON sle.item_code = i.name
+				WHERE sle.posting_date <= %s
+				AND sle.warehouse = 'Stores - SHIW'
+				AND i.item_group = 'Raw Material'
+				AND sle.is_cancelled = 0
+				""",
+				(date_str,),
+				as_list=True,
+			)[0][0]
+			or 0
+		)
+		row_rm_stock.append(stock_qty)
+	data.append(row_rm_stock)
+
+	# Row 4: Liquid Metal Generated
+	row_liquid_metal = ["Liquid Metal Generated"]
+	for day in range(1, days_in_month + 1):
+		date_str = get_date(day)
+		liquid_qty = (
+			frappe.db.sql(
+				"""
+				SELECT SUM(liquid_balence)
+				FROM `tabHeat`
+				WHERE `date` = %s
+				AND docstatus = 1
+				""",
+				(date_str,),
+				as_list=True,
+			)[0][0]
+			or 0
+		)
+		row_liquid_metal.append(liquid_qty)
+	data.append(row_liquid_metal)
+
+	return columns, data
 
 
 # import frappe
@@ -783,5 +783,27 @@ def execute(filters=None):
 # 		)
 # 		row_finishing_weight.append(finishing_weight)
 # 	data.append(row_finishing_weight)
+
+# 	row_fs_stock = ["Finished Stock Weight"]
+# 	for day in range(1, days_in_month + 1):
+# 		date_str = get_date(day)
+# 		stock_qty = (
+# 			frappe.db.sql(
+# 				"""
+# 				SELECT SUM(sle.actual_qty)
+# 				FROM `tabStock Ledger Entry` sle
+# 				JOIN `tabItem` i ON sle.item_code = i.name
+# 				WHERE sle.posting_date <= %s
+# 				AND sle.warehouse = 'FInished Good - SHIW'
+# 				AND i.item_group = 'Finished Goods'
+# 				AND sle.is_cancelled = 0
+# 				""",
+# 				(date_str,),
+# 				as_list=True,
+# 			)[0][0]
+# 			or 0
+# 		)
+# 		row_fs_stock.append(stock_qty)
+# 	data.append(row_rm_stock)
 
 # 	return columns, data
