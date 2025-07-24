63e0aee20c3a98f03f03c6c43497fe4c59b0c058
beetashoke <beetashoke.chakraborty@clapgrow.com>
2025-07-24 12:43:34 +0530

    toolingjs

--- shiw/shiw/report/comprehensive_summary_report/comprehensive_summary_report.py
+++ shiw/shiw/report/comprehensive_summary_report/comprehensive_summary_report.py
@@ -127,32 +127,32 @@ def execute(filters=None):
 
 	data.append(row_rm_melted)
 
-	# # Row 3: Raw Materials Stock (as of each day, from Stock Ledger Entry)
-	# row_rm_stock = ["Raw Materials Stock"]
+	# Row 3: Raw Materials Stock (as of each day, from Stock Ledger Entry)
+	row_rm_stock = ["Raw Materials Stock"]
 
-	# for day in range(1, days_in_month + 1):
-	# 	date_str = get_date(day)
+	for day in range(1, days_in_month + 1):
+		date_str = get_date(day)
 
-	# 	stock_qty = (
-	# 		frappe.db.sql(
-	# 			"""
-	# 			SELECT SUM(sle.actual_qty)
-	# 			FROM `tabStock Ledger Entry` sle
-	# 			JOIN `tabItem` i ON sle.item_code = i.name
-	# 			WHERE sle.posting_date <= %s
-	# 			AND sle.warehouse = 'Stores - SHIW'
-	# 			AND i.item_group = 'Raw Material'
-	# 			AND sle.is_cancelled = 0
-	# 			""",
-	# 			(date_str,),
-	# 			as_list=True,
-	# 		)[0][0]
-	# 		or 0
-	# 	)
+		stock_qty = (
+			frappe.db.sql(
+				"""
+				SELECT SUM(sle.actual_qty)
+				FROM `tabStock Ledger Entry` sle
+				JOIN `tabItem` i ON sle.item_code = i.name
+				WHERE sle.posting_date <= %s
+				AND sle.warehouse = 'Stores - SHIW'
+				AND i.item_group = 'Raw Material'
+				AND sle.is_cancelled = 0
+				""",
+				(date_str,),
+				as_list=True,
+			)[0][0]
+			or 0
+		)
 
-	# 	row_rm_stock.append(stock_qty)
+		row_rm_stock.append(stock_qty)
 
-	# data.append(row_rm_stock)
+	data.append(row_rm_stock)
 
 	return columns, data
 
@@ -555,5 +555,233 @@ def execute(filters=None):
 # 		)
 # 		row_burning_loss.append(round(loss_pct, 2))
 # 	data.append(row_burning_loss)
+
+# 	# Row 8: Co2 Mould Bunch Weight
+# 	row_bunch_weight = ["Co2 Mould Bunch Weight"]
+# 	for day in range(1, days_in_month + 1):
+# 		date_str = get_date(day)
+# 		bunch_weight = (
+# 			frappe.db.sql(
+# 				"""
+# 				SELECT SUM(total_bunch_weight)
+# 				FROM `tabCo2 Mould Batch`
+# 				WHERE `date` = %s
+# 				""",
+# 				(date_str,),
+# 				as_list=True,
+# 			)[0][0]
+# 			or 0
+# 		)
+# 		row_bunch_weight.append(bunch_weight)
+# 	data.append(row_bunch_weight)
+
+# 	# Row 9: Co2 Mould Cast Weight
+# 	row_cast_weight = ["Co2 Mould Cast Weight"]
+# 	for day in range(1, days_in_month + 1):
+# 		date_str = get_date(day)
+# 		cast_weight = (
+# 			frappe.db.sql(
+# 				"""
+# 				SELECT SUM(total_cast_weight)
+# 				FROM `tabCo2 Mould Batch`
+# 				WHERE `date` = %s
+# 				""",
+# 				(date_str,),
+# 				as_list=True,
+# 			)[0][0]
+# 			or 0
+# 		)
+# 		row_cast_weight.append(cast_weight)
+# 	data.append(row_cast_weight)
+
+# 	# Row 10: HPML Mould Cast Weight
+# 	row_hpml_cast_weight = ["HPML Mould Cast Weight"]
+# 	for day in range(1, days_in_month + 1):
+# 		date_str = get_date(day)
+# 		hpml_cast_weight = (
+# 			frappe.db.sql(
+# 				"""
+# 				SELECT SUM(total_cast_weight)
+# 				FROM `tabHPML Mould Batch`
+# 				WHERE `date` = %s
+# 				""",
+# 				(date_str,),
+# 				as_list=True,
+# 			)[0][0]
+# 			or 0
+# 		)
+# 		row_hpml_cast_weight.append(hpml_cast_weight)
+# 	data.append(row_hpml_cast_weight)
+
+# 	# Row 11: HPML Mould Bunch Weight
+# 	row_hpml_bunch_weight = ["HPML Mould Bunch Weight"]
+# 	for day in range(1, days_in_month + 1):
+# 		date_str = get_date(day)
+# 		hpml_bunch_weight = (
+# 			frappe.db.sql(
+# 				"""
+# 				SELECT SUM(total_bunch_weight)
+# 				FROM `tabHPML Mould Batch`
+# 				WHERE `date` = %s
+# 				""",
+# 				(date_str,),
+# 				as_list=True,
+# 			)[0][0]
+# 			or 0
+# 		)
+# 		row_hpml_bunch_weight.append(hpml_bunch_weight)
+# 	data.append(row_hpml_bunch_weight)
+
+# 	# Row 12: No-Bake Mould Bunch Weight
+# 	row_nb_bunch_weight = ["No-Bake Mould Bunch Weight"]
+# 	for day in range(1, days_in_month + 1):
+# 		date_str = get_date(day)
+# 		nb_bunch_weight = (
+# 			frappe.db.sql(
+# 				"""
+# 				SELECT SUM(total_bunch_weight)
+# 				FROM `tabNo-Bake Mould Batch`
+# 				WHERE `date` = %s
+# 				""",
+# 				(date_str,),
+# 				as_list=True,
+# 			)[0][0]
+# 			or 0
+# 		)
+# 		row_nb_bunch_weight.append(nb_bunch_weight)
+# 	data.append(row_nb_bunch_weight)
+
+# 	# Row 13: No-Bake Mould Cast Weight
+# 	row_nb_cast_weight = ["No-Bake Mould Cast Weight"]
+# 	for day in range(1, days_in_month + 1):
+# 		date_str = get_date(day)
+# 		nb_cast_weight = (
+# 			frappe.db.sql(
+# 				"""
+# 				SELECT SUM(total_cast_weight)
+# 				FROM `tabNo-Bake Mould Batch`
+# 				WHERE `date` = %s
+# 				""",
+# 				(date_str,),
+# 				as_list=True,
+# 			)[0][0]
+# 			or 0
+# 		)
+# 		row_nb_cast_weight.append(nb_cast_weight)
+# 	data.append(row_nb_cast_weight)
+
+# 	# Row 14: Jolt Squeeze Mould Bunch Weight
+# 	row_js_bunch_weight = ["Jolt Squeeze Mould Bunch Weight"]
+# 	for day in range(1, days_in_month + 1):
+# 		date_str = get_date(day)
+# 		js_bunch_weight = (
+# 			frappe.db.sql(
+# 				"""
+# 				SELECT SUM(total_bunch_weight)
+# 				FROM `tabJolt Squeeze Mould Batch`
+# 				WHERE `date` = %s
+# 				""",
+# 				(date_str,),
+# 				as_list=True,
+# 			)[0][0]
+# 			or 0
+# 		)
+# 		row_js_bunch_weight.append(js_bunch_weight)
+# 	data.append(row_js_bunch_weight)
+
+# 	# Row 15: Jolt Squeeze Mould Cast Weight
+# 	row_js_cast_weight = ["Jolt Squeeze Mould Cast Weight"]
+# 	for day in range(1, days_in_month + 1):
+# 		date_str = get_date(day)
+# 		js_cast_weight = (
+# 			frappe.db.sql(
+# 				"""
+# 				SELECT SUM(total_cast_weight)
+# 				FROM `tabJolt Squeeze Mould Batch`
+# 				WHERE `date` = %s
+# 				""",
+# 				(date_str,),
+# 				as_list=True,
+# 			)[0][0]
+# 			or 0
+# 		)
+# 		row_js_cast_weight.append(js_cast_weight)
+# 	data.append(row_js_cast_weight)
+
+# 	# Row 16: Green Sand Hand Mould Bunch Weight
+# 	row_gs_bunch_weight = ["Green Sand Hand Mould Bunch Weight"]
+# 	for day in range(1, days_in_month + 1):
+# 		date_str = get_date(day)
+# 		gs_bunch_weight = (
+# 			frappe.db.sql(
+# 				"""
+# 				SELECT SUM(total_bunch_weight)
+# 				FROM `tabGreen Sand Hand Mould Batch`
+# 				WHERE `date` = %s
+# 				""",
+# 				(date_str,),
+# 				as_list=True,
+# 			)[0][0]
+# 			or 0
+# 		)
+# 		row_gs_bunch_weight.append(gs_bunch_weight)
+# 	data.append(row_gs_bunch_weight)
+
+# 	# Row 17: Green Sand Hand Mould Cast Weight
+# 	row_gs_cast_weight = ["Green Sand Hand Mould Cast Weight"]
+# 	for day in range(1, days_in_month + 1):
+# 		date_str = get_date(day)
+# 		gs_cast_weight = (
+# 			frappe.db.sql(
+# 				"""
+# 				SELECT SUM(total_cast_weight)
+# 				FROM `tabGreen Sand Hand Mould Batch`
+# 				WHERE `date` = %s
+# 				""",
+# 				(date_str,),
+# 				as_list=True,
+# 			)[0][0]
+# 			or 0
+# 		)
+# 		row_gs_cast_weight.append(gs_cast_weight)
+# 	data.append(row_gs_cast_weight)
+
+# 	# Row 18: Fettling Weight
+# 	row_fettling_weight = ["Fettling Weight"]
+# 	for day in range(1, days_in_month + 1):
+# 		date_str = get_date(day)
+# 		fettling_weight = (
+# 			frappe.db.sql(
+# 				"""
+# 				SELECT SUM(total_fettling_weight)
+# 				FROM `tabFettling`
+# 				WHERE `date` = %s
+# 				""",
+# 				(date_str,),
+# 				as_list=True,
+# 			)[0][0]
+# 			or 0
+# 		)
+# 		row_fettling_weight.append(fettling_weight)
+# 	data.append(row_fettling_weight)
+
+# 	# Row 19: Finishing Weight
+# 	row_finishing_weight = ["Finishing Weight"]
+# 	for day in range(1, days_in_month + 1):
+# 		date_str = get_date(day)
+# 		finishing_weight = (
+# 			frappe.db.sql(
+# 				"""
+# 				SELECT SUM(total_finishing_weight)
+# 				FROM `tabFinishing`
+# 				WHERE `date` = %s
+# 				""",
+# 				(date_str,),
+# 				as_list=True,
+# 			)[0][0]
+# 			or 0
+# 		)
+# 		row_finishing_weight.append(finishing_weight)
+# 	data.append(row_finishing_weight)
 
 # 	return columns, data
