0234849a45582035c617e86c27750ed71293f884
beetashoke <beetashoke.chakraborty@clapgrow.com>
2025-09-01 13:21:13 +0530

    colour codeing

--- shiw/shiw/report/inventory_management_system/inventory_management_system.js
+++ shiw/shiw/report/inventory_management_system/inventory_management_system.js
@@ -47,75 +47,392 @@
 // };
 
 
+//only number colour
+// frappe.query_reports["Inventory Management System"] = {
+// 	"filters": [
+// 		{
+// 			fieldname: "custom_department",
+// 			label: "Department",
+// 			fieldtype: "Select",
+// 			options: [
+// 				"", // All
+// 				"Mould Box",
+// 				"Co2",
+// 				"Co2 Core",
+// 				"Co2 Hand Mould",
+// 				"Core",
+// 				"Core Suiter Machine",
+// 				"Fettling",
+// 				"Fettling/Finishing",
+// 				"Finished",
+// 				"Green Sand",
+// 				"Grinding",
+// 				"Gs",
+// 				"Gs Core",
+// 				"Gs Hand Mould",
+// 				"Hpml",
+// 				"Hpml Core",
+// 				"Instruments",
+// 				"Jolt Squeeze",
+// 				"Laboratory",
+// 				"Labortary",
+// 				"Maintenance",
+// 				"Match Plate",
+// 				"Melting",
+// 				"Miscellaneous",
+// 				"MiscellaneousMatch Plate",
+// 				"Moulding",
+// 				"Nobake",
+// 				"Nobake Core",
+// 				"Pattern Shop",
+// 				"Quality",
+// 				"Shakeout",
+// 				"Shortblast"
+// 			].join("\n")
+// 		},
+// 		{
+// 			fieldname: "item",
+// 			label: "Item",
+// 			fieldtype: "Link",
+// 			options: "Item",
+// 			reqd: 0
+// 		}
+// 	],
 
+// 	// Your color logic (kept as-is)
+// 	formatter: function (value, row, column, data, default_formatter) {
+// 		value = default_formatter(value, row, column, data);
+
+// 		if (column.fieldname === "store_qty" && data) {
+// 			const q = Number(data.store_qty || 0);
+// 			const minq = Number(data.custom_minimum_stock || 0);
+// 			const maxq = Number(data.custom_maximum_stock || 0);
+
+// 			if (q < minq) {
+// 				value = `<span style="color:red; font-weight:bold">${q}</span>`;
+// 			} else if (q <= maxq) {
+// 				value = `<span style="color:green; font-weight:bold">${q}</span>`;
+// 			} else {
+// 				value = `<span style="color:purple; font-weight:bold">${q}</span>`;
+// 			}
+// 		}
+// 		return value;
+// 	}
+// };
+
+
+
+
+//only number cell background colour
+// frappe.query_reports["Inventory Management System"] = {
+// 	"filters": [
+// 		{
+// 			fieldname: "custom_department",
+// 			label: "Department",
+// 			fieldtype: "Select",
+// 			options: [
+// 				"", // All
+// 				"Mould Box",
+// 				"Co2",
+// 				"Co2 Core",
+// 				"Co2 Hand Mould",
+// 				"Core",
+// 				"Core Suiter Machine",
+// 				"Fettling",
+// 				"Fettling/Finishing",
+// 				"Finished",
+// 				"Green Sand",
+// 				"Grinding",
+// 				"Gs",
+// 				"Gs Core",
+// 				"Gs Hand Mould",
+// 				"Hpml",
+// 				"Hpml Core",
+// 				"Instruments",
+// 				"Jolt Squeeze",
+// 				"Laboratory",
+// 				"Labortary",
+// 				"Maintenance",
+// 				"Match Plate",
+// 				"Melting",
+// 				"Miscellaneous",
+// 				"MiscellaneousMatch Plate",
+// 				"Moulding",
+// 				"Nobake",
+// 				"Nobake Core",
+// 				"Pattern Shop",
+// 				"Quality",
+// 				"Shakeout",
+// 				"Shortblast"
+// 			].join("\n")
+// 		},
+// 		{
+// 			fieldname: "item",
+// 			label: "Item",
+// 			fieldtype: "Link",
+// 			options: "Item",
+// 			reqd: 0
+// 		}
+// 	],
+
+// 	formatter: function (value, row, column, data, default_formatter) {
+// 		value = default_formatter(value, row, column, data);
+
+// 		if (column.fieldname === "store_qty" && data) {
+// 			const q = Number(data.store_qty || 0);
+// 			const minq = Number(data.custom_minimum_stock || 0);
+// 			const maxq = Number(data.custom_maximum_stock || 0);
+
+// 			let bgColor = "";
+// 			if (q < minq) {
+// 				bgColor = "red";
+// 			} else if (q <= maxq) {
+// 				bgColor = "lightgreen";
+// 			} else {
+// 				bgColor = "violet";
+// 			}
+
+// 			value = `<span style="background-color:${bgColor}; padding:2px 6px; border-radius:4px; display:inline-block;">${q}</span>`;
+// 		}
+// 		return value;
+// 	}
+// };
+
+
+// square colour
+// frappe.query_reports["Inventory Management System"] = {
+// 	"filters": [
+// 		{
+// 			fieldname: "custom_department",
+// 			label: "Department",
+// 			fieldtype: "Select",
+// 			options: [
+// 				"", // All
+// 				"Mould Box",
+// 				"Co2",
+// 				"Co2 Core",
+// 				"Co2 Hand Mould",
+// 				"Core",
+// 				"Core Suiter Machine",
+// 				"Fettling",
+// 				"Fettling/Finishing",
+// 				"Finished",
+// 				"Green Sand",
+// 				"Grinding",
+// 				"Gs",
+// 				"Gs Core",
+// 				"Gs Hand Mould",
+// 				"Hpml",
+// 				"Hpml Core",
+// 				"Instruments",
+// 				"Jolt Squeeze",
+// 				"Laboratory",
+// 				"Labortary",
+// 				"Maintenance",
+// 				"Match Plate",
+// 				"Melting",
+// 				"Miscellaneous",
+// 				"MiscellaneousMatch Plate",
+// 				"Moulding",
+// 				"Nobake",
+// 				"Nobake Core",
+// 				"Pattern Shop",
+// 				"Quality",
+// 				"Shakeout",
+// 				"Shortblast"
+// 			].join("\n")
+// 		},
+// 		{
+// 			fieldname: "item",
+// 			label: "Item",
+// 			fieldtype: "Link",
+// 			options: "Item",
+// 			reqd: 0
+// 		}
+// 	],
+
+// 	formatter: function (value, row, column, data, default_formatter) {
+// 		value = default_formatter(value, row, column, data);
+
+// 		if (column.fieldname === "store_qty" && data) {
+// 			const q = Number(data.store_qty || 0);
+// 			const minq = Number(data.custom_minimum_stock || 0);
+// 			const maxq = Number(data.custom_maximum_stock || 0);
+
+// 			let bgColor = "";
+// 			let textColor = "black";
+
+// 			if (q < minq) {
+// 				bgColor = "red";
+// 				textColor = "white";
+// 			} else if (q <= maxq) {
+// 				bgColor = "lightgreen";
+// 				textColor = "black";
+// 			} else {
+// 				bgColor = "violet";
+// 				textColor = "white";
+// 			}
+
+// 			// Full cell background color
+// 			value = `<div style="
+// 				background-color:${bgColor};
+// 				color:${textColor};
+// 				width:100%;
+// 				height:100%;
+// 				padding:4px;
+// 				box-sizing:border-box;
+// 			">${q}</div>`;
+// 		}
+// 		return value;
+// 	}
+// };
+
+
+
+//rounded colour
+// frappe.query_reports["Inventory Management System"] = {
+// 	"filters": [
+// 		{
+// 			fieldname: "custom_department",
+// 			label: "Department",
+// 			fieldtype: "Select",
+// 			options: [
+// 				"", // All
+// 				"Mould Box",
+// 				"Co2",
+// 				"Co2 Core",
+// 				"Co2 Hand Mould",
+// 				"Core",
+// 				"Core Suiter Machine",
+// 				"Fettling",
+// 				"Fettling/Finishing",
+// 				"Finished",
+// 				"Green Sand",
+// 				"Grinding",
+// 				"Gs",
+// 				"Gs Core",
+// 				"Gs Hand Mould",
+// 				"Hpml",
+// 				"Hpml Core",
+// 				"Instruments",
+// 				"Jolt Squeeze",
+// 				"Laboratory",
+// 				"Labortary",
+// 				"Maintenance",
+// 				"Match Plate",
+// 				"Melting",
+// 				"Miscellaneous",
+// 				"MiscellaneousMatch Plate",
+// 				"Moulding",
+// 				"Nobake",
+// 				"Nobake Core",
+// 				"Pattern Shop",
+// 				"Quality",
+// 				"Shakeout",
+// 				"Shortblast"
+// 			].join("\n")
+// 		},
+// 		{
+// 			fieldname: "item",
+// 			label: "Item",
+// 			fieldtype: "Link",
+// 			options: "Item",
+// 			reqd: 0
+// 		}
+// 	],
+
+// 	formatter: function (value, row, column, data, default_formatter) {
+// 		value = default_formatter(value, row, column, data);
+
+// 		if (column.fieldname === "store_qty" && data) {
+// 			const q = Number(data.store_qty || 0);
+// 			const minq = Number(data.custom_minimum_stock || 0);
+// 			const maxq = Number(data.custom_maximum_stock || 0);
+
+// 			let bgColor = "";
+// 			let textColor = "black";
+// 			if (q < minq) {
+// 				bgColor = "red";
+// 				textColor = "white";
+// 			} else if (q <= maxq) {
+// 				bgColor = "lightgreen";
+// 				textColor = "black";
+// 			} else {
+// 				bgColor = "violet";
+// 				textColor = "white";
+// 			}
+
+// 			// full cell background with rounded corners
+// 			value = `<div style="
+// 				background-color:${bgColor};
+// 				color:${textColor};
+// 				width:100%;
+// 				height:100%;
+// 				padding:4px;
+// 				box-sizing:border-box;
+// 				border-radius:8px;">
+// 				${q}
+// 				</div>`;
+// 		}
+// 		return value;
+// 	}
+// };
+
+
+
+
+
+//final script
 frappe.query_reports["Inventory Management System"] = {
-	"filters": [
-		{
-			fieldname: "custom_department",
-			label: "Department",
-			fieldtype: "Select",
-			options: [
-				"", // All
-				"Mould Box",
-				"Co2",
-				"Co2 Core",
-				"Co2 Hand Mould",
-				"Core",
-				"Core Suiter Machine",
-				"Fettling",
-				"Fettling/Finishing",
-				"Finished",
-				"Green Sand",
-				"Grinding",
-				"Gs",
-				"Gs Core",
-				"Gs Hand Mould",
-				"Hpml",
-				"Hpml Core",
-				"Instruments",
-				"Jolt Squeeze",
-				"Laboratory",
-				"Labortary",
-				"Maintenance",
-				"Match Plate",
-				"Melting",
-				"Miscellaneous",
-				"MiscellaneousMatch Plate",
-				"Moulding",
-				"Nobake",
-				"Nobake Core",
-				"Pattern Shop",
-				"Quality",
-				"Shakeout",
-				"Shortblast"
-			].join("\n")
-		},
-		{
-			fieldname: "item",
-			label: "Item",
-			fieldtype: "Link",
-			options: "Item",
-			reqd: 0
-		}
-	],
+    filters: [
+        {
+            fieldname: "custom_department",
+            label: "Department",
+            fieldtype: "Select",
+            options: [
+                "",
+                "Mould Box", "Co2", "Co2 Core", "Co2 Hand Mould", "Core",
+                "Core Suiter Machine", "Fettling", "Fettling/Finishing",
+                "Finished", "Green Sand", "Grinding", "Gs", "Gs Core", "Gs Hand Mould",
+                "Hpml", "Hpml Core", "Instruments", "Jolt Squeeze", "Laboratory",
+                "Labortary", "Maintenance", "Match Plate", "Melting", "Miscellaneous",
+                "MiscellaneousMatch Plate", "Moulding", "Nobake", "Nobake Core",
+                "Pattern Shop", "Quality", "Shakeout", "Shortblast"
+            ],
+            width: "180"
+        },
+        {
+            fieldname: "item",
+            label: "Item",
+            fieldtype: "Link",
+            options: "Item",
+            width: "220"
+        },
+        {
+            fieldname: "color_filter",
+            label: "Color",
+            fieldtype: "Select",
+            options: ["", "Red", "Green", "Purple"], // matches Python "status"
+            width: "120"
+        }
+    ],
 
-	// Your color logic (kept as-is)
-	formatter: function (value, row, column, data, default_formatter) {
-		value = default_formatter(value, row, column, data);
+    formatter: function (value, row, column, data, default_formatter) {
+        value = default_formatter(value, row, column, data);
 
-		if (column.fieldname === "store_qty" && data) {
-			const q = Number(data.store_qty || 0);
-			const minq = Number(data.custom_minimum_stock || 0);
-			const maxq = Number(data.custom_maximum_stock || 0);
+        if (column.fieldname === "store_qty" && data) {
+            let bg = "";
+            if (data.status === "Red") bg = "#ff4d4d";       // red
+            else if (data.status === "Green") bg = "#4dff88"; // green
+            else if (data.status === "Purple") bg = "#b84dff"; // purple
 
-			if (q < minq) {
-				value = `<span style="color:red; font-weight:bold">${q}</span>`;
-			} else if (q <= maxq) {
-				value = `<span style="color:green; font-weight:bold">${q}</span>`;
-			} else {
-				value = `<span style="color:purple; font-weight:bold">${q}</span>`;
-			}
-		}
-		return value;
-	}
+            return `<div style="background-color:${bg}; 
+                                border-radius:12px; 
+                                padding:4px; 
+                                text-align:center;">
+                        ${data.store_qty}
+                    </div>`;
+        }
+        return value;
+    }
 };
--- shiw/shiw/report/inventory_management_system/inventory_management_system.py
+++ shiw/shiw/report/inventory_management_system/inventory_management_system.py
@@ -1,72 +1,14 @@
 # Copyright (c) 2025, beetashoke chakraborty and contributors
 # For license information, please see license.txt
 
-# import frappe
-
-
-# def execute(filters=None):
-# 	columns, data = [], []
-# 	return columns, data
-
 
 
 # import frappe
 
-# def execute(filters=None):
-#     columns = get_columns()
-#     data = get_data()
-#     return columns, data
-
-# def get_columns():
-#     return [
-#         {"label": "Department", "fieldname": "custom_department", "fieldtype": "Data", "width": 150},
-#         {"label": "Item Name", "fieldname": "item_name", "fieldtype": "Data", "width": 200},
-#         {"label": "UOM", "fieldname": "uom", "fieldtype": "Data", "width": 100},
-#         {"label": "Store Qty", "fieldname": "store_qty", "fieldtype": "Float", "width": 120},
-#         {"label": "Min Qty", "fieldname": "custom_minimum_stock", "fieldtype": "Float", "width": 120},
-#         {"label": "Max Qty", "fieldname": "custom_maximum_stock", "fieldtype": "Float", "width": 120},
-#     ]
-
-# def get_data():
-#     # Fetch Items with required fields
-#     items = frappe.get_all(
-#         "Item",
-#         fields=[
-#             "name",
-#             "item_name",
-#             "stock_uom as uom",
-#             "custom_department",
-#             "custom_minimum_stock",
-#             "custom_maximum_stock"
-#         ]
-#     )
-
-#     data = []
-#     for item in items:
-#         # Fetch available quantity in "Stores - SHIW"
-#         store_qty = frappe.db.get_value(
-#             "Bin",
-#             {"item_code": item.name, "warehouse": "Stores - SHIW"},
-#             "actual_qty"
-#         ) or 0
-
-#         data.append({
-#             "custom_department": item.custom_department,
-#             "item_name": item.item_name,
-#             "uom": item.uom,
-#             "store_qty": store_qty,
-#             "custom_minimum_stock": item.custom_minimum_stock,
-#             "custom_maximum_stock": item.custom_maximum_stock
-#         })
-
-#     return data
-
-
-
-
-# import frappe
+# WAREHOUSE = "Stores - SHIW"
 
 # def execute(filters=None):
+#     filters = filters or {}
 #     columns = get_columns()
 #     data = get_data(filters)
 #     return columns, data
@@ -74,53 +16,200 @@
 # def get_columns():
 #     return [
 #         {"label": "Department", "fieldname": "custom_department", "fieldtype": "Data", "width": 150},
-#         {"label": "Item Name", "fieldname": "item_name", "fieldtype": "Data", "width": 200},
-#         {"label": "UOM", "fieldname": "uom", "fieldtype": "Data", "width": 100},
+#         {"label": "Item Name", "fieldname": "item_name", "fieldtype": "Data", "width": 220},
+#         {"label": "UOM", "fieldname": "uom", "fieldtype": "Data", "width": 90},
 #         {"label": "Store Qty", "fieldname": "store_qty", "fieldtype": "Float", "width": 120},
-#         {"label": "Min Qty", "fieldname": "custom_minimum_stock", "fieldtype": "Float", "width": 120},
-#         {"label": "Max Qty", "fieldname": "custom_maximum_stock", "fieldtype": "Float", "width": 120},
+#         {"label": "Min Qty", "fieldname": "custom_minimum_stock", "fieldtype": "Float", "width": 110},
+#         {"label": "Max Qty", "fieldname": "custom_maximum_stock", "fieldtype": "Float", "width": 110},
 #     ]
 
 # def get_data(filters):
-#     conditions = {}
+#     # Build filter list properly (avoid dict-with-operators issues)
+#     flt_list = []
 #     if filters.get("custom_department"):
-#         conditions["custom_department"] = filters["custom_department"]
-#     if filters.get("item_name"):
-#         conditions["item_name"] = ["like", f"%{filters['item_name']}%"]
+#         flt_list.append(["Item", "custom_department", "=", filters["custom_department"]])
+#     if filters.get("item"):  # Link to Item returns the Item's name (item_code)
+#         flt_list.append(["Item", "name", "=", filters["item"]])
 
 #     items = frappe.get_all(
 #         "Item",
-#         filters=conditions,
+#         filters=flt_list,
 #         fields=[
 #             "name",
 #             "item_name",
 #             "stock_uom as uom",
 #             "custom_department",
 #             "custom_minimum_stock",
-#             "custom_maximum_stock"
-#         ]
+#             "custom_maximum_stock",
+#         ],
+#         order_by="item_name asc"
 #     )
 
 #     data = []
-#     for item in items:
-#         # Fetch available quantity in "Stores - SHIW"
+#     for it in items:
 #         store_qty = frappe.db.get_value(
-#             "Bin",
-#             {"item_code": item.name, "warehouse": "Stores - SHIW"},
-#             "actual_qty"
+#             "Bin", {"item_code": it.name, "warehouse": WAREHOUSE}, "actual_qty"
 #         ) or 0
 
 #         data.append({
-#             "custom_department": item.custom_department,
-#             "item_name": item.item_name,
-#             "uom": item.uom,
+#             "custom_department": it.custom_department,
+#             "item_name": it.item_name,
+#             "uom": it.uom,
 #             "store_qty": store_qty,
-#             "custom_minimum_stock": item.custom_minimum_stock,
-#             "custom_maximum_stock": item.custom_maximum_stock
+#             "custom_minimum_stock": it.custom_minimum_stock,
+#             "custom_maximum_stock": it.custom_maximum_stock,
 #         })
-
 #     return data
 
+
+
+# import frappe
+
+# WAREHOUSE = "Stores - SHIW"
+
+# def execute(filters=None):
+#     filters = filters or {}
+#     columns = get_columns()
+#     data = get_data(filters)
+#     return columns, data
+
+
+# def get_columns():
+#     return [
+#         {"label": "Department", "fieldname": "custom_department", "fieldtype": "Data", "width": 150},
+#         {"label": "Item Name", "fieldname": "item_name", "fieldtype": "Data", "width": 220},
+#         {"label": "UOM", "fieldname": "uom", "fieldtype": "Data", "width": 90},
+#         {"label": "Store Qty", "fieldname": "store_qty", "fieldtype": "Float", "width": 120},
+#         {"label": "Min Qty", "fieldname": "custom_minimum_stock", "fieldtype": "Float", "width": 110},
+#         {"label": "Max Qty", "fieldname": "custom_maximum_stock", "fieldtype": "Float", "width": 110},
+#     ]
+
+
+# def get_data(filters):
+#     flt_list = []
+#     if filters.get("custom_department"):
+#         flt_list.append(["Item", "custom_department", "=", filters["custom_department"]])
+#     if filters.get("item"):
+#         flt_list.append(["Item", "name", "=", filters["item"]])
+
+#     items = frappe.get_all(
+#         "Item",
+#         filters=flt_list,
+#         fields=[
+#             "name",
+#             "item_name",
+#             "stock_uom as uom",
+#             "custom_department",
+#             "custom_minimum_stock",
+#             "custom_maximum_stock",
+#         ],
+#         order_by="item_name asc"
+#     )
+
+#     data = []
+#     for it in items:
+#         store_qty = frappe.db.get_value(
+#             "Bin", {"item_code": it.name, "warehouse": WAREHOUSE}, "actual_qty"
+#         ) or 0
+
+#         # Determine color status
+#         color_status = "green"
+#         if it.custom_minimum_stock and store_qty < it.custom_minimum_stock:
+#             color_status = "red"
+#         elif it.custom_maximum_stock and store_qty > it.custom_maximum_stock:
+#             color_status = "purple"
+
+#         # Apply filter for color if chosen
+#         if filters.get("color_status") and filters["color_status"] != color_status:
+#             continue
+
+#         data.append({
+#             "custom_department": it.custom_department,
+#             "item_name": it.item_name,
+#             "uom": it.uom,
+#             "store_qty": store_qty,
+#             "custom_minimum_stock": it.custom_minimum_stock,
+#             "custom_maximum_stock": it.custom_maximum_stock,
+#         })
+#     return data
+
+
+
+
+# import frappe
+
+# WAREHOUSE = "Stores - SHIW"
+
+# def execute(filters=None):
+#     filters = filters or {}
+#     columns = get_columns()
+#     data = get_data(filters)
+
+#     # If user selected a "color filter", apply it here
+#     if filters.get("color_filter"):
+#         data = [row for row in data if row["status"] == filters["color_filter"]]
+
+#     return columns, data
+
+
+# def get_columns():
+#     return [
+#         {"label": "Department", "fieldname": "custom_department", "fieldtype": "Data", "width": 150},
+#         {"label": "Item Name", "fieldname": "item_name", "fieldtype": "Data", "width": 220},
+#         {"label": "UOM", "fieldname": "uom", "fieldtype": "Data", "width": 90},
+#         {"label": "Store Qty", "fieldname": "store_qty", "fieldtype": "Float", "width": 120},
+#         {"label": "Min Qty", "fieldname": "custom_minimum_stock", "fieldtype": "Float", "width": 110},
+#         {"label": "Max Qty", "fieldname": "custom_maximum_stock", "fieldtype": "Float", "width": 110},
+#         # hidden helper column
+#         {"label": "Status", "fieldname": "status", "fieldtype": "Data", "hidden": 1},
+#     ]
+
+
+# def get_data(filters):
+#     flt_list = []
+#     if filters.get("custom_department"):
+#         flt_list.append(["Item", "custom_department", "=", filters["custom_department"]])
+#     if filters.get("item"):  
+#         flt_list.append(["Item", "name", "=", filters["item"]])
+
+#     items = frappe.get_all(
+#         "Item",
+#         filters=flt_list,
+#         fields=[
+#             "name",
+#             "item_name",
+#             "stock_uom as uom",
+#             "custom_department",
+#             "custom_minimum_stock",
+#             "custom_maximum_stock",
+#         ],
+#         order_by="item_name asc"
+#     )
+
+#     data = []
+#     for it in items:
+#         store_qty = frappe.db.get_value(
+#             "Bin", {"item_code": it.name, "warehouse": WAREHOUSE}, "actual_qty"
+#         ) or 0
+
+#         # Determine status
+#         if store_qty < (it.custom_minimum_stock or 0):
+#             status = "Red"
+#         elif (it.custom_minimum_stock or 0) <= store_qty <= (it.custom_maximum_stock or 0):
+#             status = "Green"
+#         else:
+#             status = "Purple"
+
+#         data.append({
+#             "custom_department": it.custom_department,
+#             "item_name": it.item_name,
+#             "uom": it.uom,
+#             "store_qty": store_qty,
+#             "custom_minimum_stock": it.custom_minimum_stock,
+#             "custom_maximum_stock": it.custom_maximum_stock,
+#             "status": status,  # hidden column for filtering
+#         })
+#     return data
 
 
 import frappe
@@ -131,7 +220,13 @@ def execute(filters=None):
     filters = filters or {}
     columns = get_columns()
     data = get_data(filters)
+
+    # If user selected a "color filter", apply it here
+    if filters.get("color_filter"):
+        data = [row for row in data if row["status"] == filters["color_filter"]]
+
     return columns, data
+
 
 def get_columns():
     return [
@@ -141,14 +236,17 @@ def get_columns():
         {"label": "Store Qty", "fieldname": "store_qty", "fieldtype": "Float", "width": 120},
         {"label": "Min Qty", "fieldname": "custom_minimum_stock", "fieldtype": "Float", "width": 110},
         {"label": "Max Qty", "fieldname": "custom_maximum_stock", "fieldtype": "Float", "width": 110},
+        {"label": "Percentage", "fieldname": "percentage", "fieldtype": "Float", "precision": 2, "width": 120},
+        # hidden helper column
+        {"label": "Status", "fieldname": "status", "fieldtype": "Data", "hidden": 1},
     ]
 
+
 def get_data(filters):
-    # Build filter list properly (avoid dict-with-operators issues)
     flt_list = []
     if filters.get("custom_department"):
         flt_list.append(["Item", "custom_department", "=", filters["custom_department"]])
-    if filters.get("item"):  # Link to Item returns the Item's name (item_code)
+    if filters.get("item"):  
         flt_list.append(["Item", "name", "=", filters["item"]])
 
     items = frappe.get_all(
@@ -171,12 +269,30 @@ def get_data(filters):
             "Bin", {"item_code": it.name, "warehouse": WAREHOUSE}, "actual_qty"
         ) or 0
 
+        min_qty = it.custom_minimum_stock or 0
+        max_qty = it.custom_maximum_stock or 0
+
+        # Percentage Calculation
+        percentage = 0
+        if max_qty > 0:
+            percentage = (store_qty / max_qty) * 100
+
+        # Determine status
+        if store_qty < min_qty:
+            status = "Red"
+        elif min_qty <= store_qty <= max_qty:
+            status = "Green"
+        else:
+            status = "Purple"
+
         data.append({
             "custom_department": it.custom_department,
             "item_name": it.item_name,
             "uom": it.uom,
             "store_qty": store_qty,
-            "custom_minimum_stock": it.custom_minimum_stock,
-            "custom_maximum_stock": it.custom_maximum_stock,
+            "custom_minimum_stock": min_qty,
+            "custom_maximum_stock": max_qty,
+            "percentage": percentage,
+            "status": status,  # hidden column for filtering
         })
     return data
