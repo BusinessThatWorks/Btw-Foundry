08e8c8c524fa51e4b2180f47d5fc9c0af3df27a4
JaiDeb <erp@clapgrow.com>
2025-09-03 15:21:11 +0530

    Beetashoke

--- shiw/shiw/doctype/heat/heat.js
+++ shiw/shiw/doctype/heat/heat.js
@@ -1,5 +1,5 @@
 // Copyright (c) 2025, beetashoke chakraborty and contributors
-// For license information, please see license.txt
+// For license information, please  license.txt
 
 // frappe.ui.form.on("Heat", {
 //     refresh(frm) {
