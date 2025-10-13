4464c08c1a19ef09fdc123d6aafd4e777b51047d
beetashoke <beetashoke.chakraborty@clapgrow.com>
2025-10-13 13:59:46 +0530

    error solved

--- shiw/shiw/page/custom_asset_repair_dashboard/__init__.py
+++ shiw/shiw/page/custom_asset_repair_dashboard/__init__.py
@@ -1,4 +1,5 @@
 # Copyright (c) 2025, beetashoke chakraborty and contributors
 # For license information, please see license.txt
 
-# This file makes the page directory a Python package
+# This file makes the custom_asset_repair_dashboard directory a Python package
+
